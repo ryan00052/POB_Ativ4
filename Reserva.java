48e94113f4b51987e7abc3dfd1ede11a1212520e
ryan00052 <164943060+ryan00052@users.noreply.github.com>
2024-05-02 12:06:12 -0300

    Create Reserva.java

---
+++ Reserva.java
@@ -0,0 +1,38 @@
+package bergue;
+
+public class Reserva {
+	private String IdCliente;
+	private int  IdVaga, IdReserva, DataIni, DataFim;
+	public String getIdCliente() {
+		return IdCliente;
+	}
+	public void setIdCliente(String idCliente) {
+		IdCliente = idCliente;
+	}
+	public int getIdVaga() {
+		return IdVaga;
+	}
+	public void setIdVaga(int idVaga) {
+		IdVaga = idVaga;
+	}
+	public int getIdReserva() {
+		return IdReserva;
+	}
+	public void setIdReserva(int idReserva) {
+		IdReserva = idReserva;
+	}
+	public int getDataIni() {
+		return DataIni;
+	}
+	public void setDataIni(int dataIni) {
+		DataIni = dataIni;
+	}
+	public int getDataFim() {
+		return DataFim;
+	}
+	public void setDataFim(int dataFim) {
+		DataFim = dataFim;
+	}
+	
+	
+}
