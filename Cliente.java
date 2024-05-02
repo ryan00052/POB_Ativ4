b2cbc836d8123695b816a367694f6391e9d5db18
ryan00052 <164943060+ryan00052@users.noreply.github.com>
2024-05-02 12:05:36 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,45 @@
+package bergue;
+
+public class Cliente {
+	private String RG, CPF, Ende, Email, Nome;
+	private double telefone;
+	
+	public String getRG() {
+		return RG;
+	}
+	public void setRG(String rG) {
+		RG = rG;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getEnde() {
+		return Ende;
+	}
+	public void setEnde(String ende) {
+		Ende = ende;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public double getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(double telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
